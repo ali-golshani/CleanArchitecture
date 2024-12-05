11179cf3f030cac847ca0ef35454bd9bea3bc05a
Ali Golshani <ali.golshani@hotmail.com>
2024-12-05 12:52:50 +0330

    GlobalSettings

--- src/Configuration/CleanArchitecture.Configurations/GlobalSettings.cs
+++ src/Configuration/CleanArchitecture.Configurations/GlobalSettings.cs
@@ -15,13 +15,6 @@ public static class GlobalSettings
         }
     }
 
-    public static class Audit
-    {
-        public const int MaxLengthOfAuditTrailResponse = 100_000;
-        public static readonly bool LogAllRequests = false;
-        public static readonly TimeSpan QueryResponseTimeThreshold = TimeSpan.FromSeconds(1);
-    }
-
     public static class IntegrationEvents
     {
         public const int MaximumNumberOfRetries = 5;
