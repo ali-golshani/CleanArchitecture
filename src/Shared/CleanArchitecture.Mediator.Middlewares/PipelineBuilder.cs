2bf5961b45f404bf5513317b69b01e8c9cadfb4b
golshani.a <golshani.a@ime.co.ir>
2024-12-21 14:26:23 +0330

    rename

--- src/Shared/CleanArchitecture.Mediator.Middlewares/PipelineBuilder.cs
+++ src/Shared/CleanArchitecture.Mediator.Middlewares/PipelineBuilder.cs
@@ -6,11 +6,11 @@ public static class PipelineBuilder
 {
     public static IRequestProcessor<TRequest, TResponse> EntryProcessor<TRequest, TResponse>(
         IRequestProcessor<TRequest, TResponse> processor,
-        params IMiddleware<TRequest, TResponse>[] filters)
+        params IMiddleware<TRequest, TResponse>[] middlewares)
     {
         IRequestProcessor<TRequest, TResponse> pipe = new LastPipe<TRequest, TResponse>(processor);
 
-        foreach (var filter in filters.Reverse())
+        foreach (var filter in middlewares.Reverse())
         {
             pipe = new FilterPipe<TRequest, TResponse>(filter, pipe);
         }
