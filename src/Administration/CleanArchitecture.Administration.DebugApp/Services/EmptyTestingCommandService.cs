6a676dd273b3dbbfadb96edba4715a56abfc4406
golshani.a <golshani.a@ime.co.ir>
2024-12-21 16:52:31 +0330

    EmptyTestingCommandService

---
+++ src/Administration/CleanArchitecture.Administration.DebugApp/Services/EmptyTestingCommandService.cs
@@ -0,0 +1,21 @@
+﻿namespace CleanArchitecture.Administration.DebugApp.Services;
+
+public class EmptyTestingCommandService(IServiceProvider serviceProvider) : ServiceBase(serviceProvider)
+{
+    public virtual async Task Run()
+    {
+        var service = CommandService();
+
+        var result = await service.Handle(Actor, new Ordering.Commands.EmptyTestingCommand.Command
+        {
+            Id = 101,
+        }, default);
+
+        if (result.IsFailure)
+        {
+            Console.WriteLine(result.Errors);
+        }
+
+        WaitingForUserInput("Press Enter to Exit ...");
+    }
+}
