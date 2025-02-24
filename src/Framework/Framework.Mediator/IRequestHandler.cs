404b5a02c80c91961052caa9ccdbbb0a7161dce5
golshani.a <golshani.a@ime.co.ir>
2025-02-24 12:47:44 +0330

    IRequestHandler files

--- src/Framework/Framework.Mediator/IRequestHandler.cs
+++ src/Framework/Framework.Mediator/IRequestHandler.cs
@@ -1,11 +1,5 @@
 ﻿namespace Framework.Mediator;
 
-public interface IRequestHandler<in TRequest, TResponse>
-    where TRequest : IRequest<TRequest, TResponse>
-{
-    Task<Result<TResponse>> Handle(TRequest request, CancellationToken cancellationToken);
-}
-
 public interface IRequestHandler
 {
     Task<Result<TResponse>> Handle<TRequest, TResponse>(IRequest<TRequest, TResponse> request, CancellationToken cancellationToken)
