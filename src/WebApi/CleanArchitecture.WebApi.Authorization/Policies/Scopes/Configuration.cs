5224193ce1369d8e24058b2be188226df99e7832
Ali Golshani <ali.golshani@hotmail.com>
2025-05-17 21:19:59 +0330

    Authorization.Policies: PolicyName

--- src/WebApi/CleanArchitecture.WebApi.Authorization/Policies/Scopes/Configuration.cs
+++ src/WebApi/CleanArchitecture.WebApi.Authorization/Policies/Scopes/Configuration.cs
@@ -9,7 +9,7 @@ internal static class Configuration
 
     public static string PolicyName(Scopes scopes)
     {
-        return $"ScopePolicy-{(int)scopes}";
+        return $"ScopePolicy-{(long)scopes}";
     }
 
     public static string ScopeName(Scopes scopes)
