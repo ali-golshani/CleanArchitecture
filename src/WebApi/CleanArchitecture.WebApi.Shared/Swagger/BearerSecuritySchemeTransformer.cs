ad26791f03a9bb28a55c752068aac21351a08550
Ali Golshani <ali.golshani@live.com>
2025-12-11 10:46:57 +0330

    code format: BearerSecuritySchemeTransformer

--- src/WebApi/CleanArchitecture.WebApi.Shared/Swagger/BearerSecuritySchemeTransformer.cs
+++ src/WebApi/CleanArchitecture.WebApi.Shared/Swagger/BearerSecuritySchemeTransformer.cs
@@ -5,6 +5,7 @@ namespace CleanArchitecture.WebApi.Shared.Swagger;
 
 internal sealed class BearerSecuritySchemeTransformer : IOpenApiDocumentTransformer
 {
+    private const string Lines = "\r\n\r\n";
     private const string SampleTokenA = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3NjA3MzI4NDAsImV4cCI6MTkxODQ5OTI0MCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiYWxpZ29sc2hhbmkiLCJSb2xlIjoicHJvZ3JhbW1lciIsIm5hbWUiOiJhbGlnb2xzaGFuaSIsImRpc3BsYXlOYW1lIjoiQWxpIEdvbHNoYW5pIiwicGVybWlzc2lvbiI6WyJSZWFkT3JkZXJzIiwiUmVnaXN0ZXJPcmRlciJdfQ.D5AF-lUqVOF3ZKJJzSXAV83t97YcvZlOzKwucAG3N6Q";
     private const string SampleTokenB = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJPbmxpbmUgSldUIEJ1aWxkZXIiLCJpYXQiOjE3NjA3MzI4NDAsImV4cCI6MTkxODQ5OTI0MCwiYXVkIjoid3d3LmV4YW1wbGUuY29tIiwic3ViIjoiYWxpZ29sc2hhbmkiLCJSb2xlIjoicHJvZ3JhbW1lciIsIm5hbWUiOiJhbGlnb2xzaGFuaSIsImRpc3BsYXlOYW1lIjoiQWxpIEdvbHNoYW5pIiwicGVybWlzc2lvbiI6WyJSZWFkT3JkZXJzIiwiUmVnaXN0ZXJPcmRlciJdfQ.W6Dta8f8fsgrMKk6mVAgft0Gk9MxJQytj_0TC4deYfY";
 
@@ -37,15 +38,8 @@ internal sealed class BearerSecuritySchemeTransformer : IOpenApiDocumentTransfor
             BearerFormat = "JWT",
             Scheme = "Bearer",
             Description =
-                "Input bearer token to access this API. Example:" +
-                "\r\n\r\n" +
-                "Schema-A:" +
-                "\r\n\r\n" +
-                SampleTokenA +
-                "\r\n\r\n" +
-                "Schema-B:" +
-                "\r\n\r\n" +
-                SampleTokenB,
+                $"Input bearer token to access this API. Example:" +
+                $"{Lines}Schema-A:{Lines}{SampleTokenA}{Lines}Schema-B:{Lines}{SampleTokenB}",
         };
     }
 }
